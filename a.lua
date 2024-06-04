281cb36b0c3418c99871719ed7e2d29561f02f9d
The DV7(s) <167229955+xyzkyle@users.noreply.github.com>
2024-06-04 18:07:21 +0500

    Create a.lua

---
+++ a.lua
@@ -0,0 +1,5 @@
+lmao=nil;w=print;x=os.sleep;e=os.exit;apipath="xyzkyle/dv7.api.lol"if apipath==apipath then end;local function a()os.sleep(2000)end;a()print([[ 
+Any tamper or skidding will result in a global ban..
+  ]])a()local b=table.concat;b=function()a()end;a()local c=[[ 
+   Any second attempt to tamper or skidding will result in a gloal perma-ban 
+]]print(c)dv7={}e()
