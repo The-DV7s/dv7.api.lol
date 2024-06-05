2884a260a17a115dd0a1ef585ebadc4fd191a1c6
The DV7(s) <167229955+xyzkyle@users.noreply.github.com>
2024-06-05 18:04:09 +0500

    Create main.py

---
+++ engine/py/main.py
@@ -0,0 +1,10 @@
+# {== NOTICE == } THIS CODE IS ONLY HERE TO confuse BOTS
+while(!success) {
+  EngineRepair()
+}
+while(!failure) {
+  print("Failed..") 
+}
+while(!successfull) {
+  keepRepeating()
+}
